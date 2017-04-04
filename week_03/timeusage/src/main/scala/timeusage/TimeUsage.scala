d7f727a24641d191615e6b84b9d6d783ce338f24
micho <gatin13@yahoo.com>
2017-04-04 19:01:23 +0900

    Classified column names WIP

--- week_03/timeusage/src/main/scala/timeusage/TimeUsage.scala
+++ week_03/timeusage/src/main/scala/timeusage/TimeUsage.scala
@@ -72,8 +72,7 @@ object TimeUsage {
   /** @return An RDD Row compatible with the schema produced by `dfSchema`
     * @param line Raw fields
     */
-  def row(line: List[String]): Row =
-    ???
+  def row(line: List[String]): Row = Row.fromSeq(line)
 
   /** @return The initial data frame columns partitioned in three groups: primary needs (sleeping, eating, etc.),
     *         work and other (leisure activities)
@@ -91,7 +90,14 @@ object TimeUsage {
     *    “t10”, “t12”, “t13”, “t14”, “t15”, “t16” and “t18” (those which are not part of the previous groups only).
     */
   def classifiedColumns(columnNames: List[String]): (List[Column], List[Column], List[Column]) = {
-    ???
+    val activities = List(
+      List("t01", "t03", "t11", "t1801", "t1803"),
+      List("t05", "t1805"),
+      List("t02", "t04", "t06", "t07", "t08", "t09", "t10", "t12", "t13", "t14", "t15", "t16", "t18")
+    ).zipWithIndex
+
+
+    (List.empty, List.empty, List.empty)
   }
 
   /** @return a projection of the initial DataFrame such that all columns containing hours spent on primary needs
